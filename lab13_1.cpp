8458f635982f4bb8f66cc87ed71866fb76c7bd6e
KarnPrasai <karn_prasai@cmu.ac.th>
2024-01-02 16:21:36 +0700

    lab13_1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,50 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int x,double B[6])
+{
+    double sum;
+    for(int i = 0;i < x;i++)
+    {
+        sum += A[i];
+    }
+    B[0] = sum/x;
+
+
+    double sum2;
+    for(int j = 0;j < x;j++)
+    {
+        sum2 += pow(A[j]-B[0],2);
+    }
+    B[1] = sqrt(sum2/x);
+    
+    double sum3 = 1;
+    for(int i = 0;i<x;i++)
+    {
+        sum3 *= A[i];
+    }
+    
+    B[2] = pow(sum3 ,(float)1/x);
+
+    double sum4;
+    for(int i = 0;i < x;i++)
+    {
+        sum4 += 1/A[i];
+    }
+    B[3] = x/sum4;
+
+    B[4] = A[0];
+    for(int k = 0;k<x;k++)
+    {
+        if(B[4] < A[k])
+            B[4] = A[k];
+    }
+
+    B[5] = A[0];
+    for(int k = 0;k<x;k++)
+    {
+        if(B[5] > A[k])
+            B[5] = A[k];
+    }
+}
